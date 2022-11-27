0000665863460d35abba96fd1b1404b15c8d647e
Evgenia Grrigorova <evgeniq.grigorova99@gmail.com>
2022-11-27 20:17:18 +0200

    uploading files from first upr

---
+++ src/hero/hero.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { HeroDto } from './hero.dto';
+import { HeroService } from './hero.service';
+
+@Controller('hero')
+export class HeroController {
+  constructor(private heroService: HeroService) {}
+
+  @Get()
+  async getAllHeroes() {
+    const heroes = await this.heroService.findAll();
+    return heroes;
+  }
+
+  @Post()
+  async createOne(@Body() hero: HeroDto) {
+    const heroes = await this.heroService.createOne(hero);
+    return heroes;
+  }
+}
